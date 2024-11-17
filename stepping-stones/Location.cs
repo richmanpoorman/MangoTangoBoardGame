2ecfc492491f570a59c18425e5c4121f711f1d4b
JlBrockett <jlbrockett@gmail.com>
2024-11-17 13:08:10 -0500

    basic implementationn for variable size board and location

---
+++ stepping-stones/Location.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class Location : Node
+{
+	private int x;
+	private int y;
+	public Location(int X, int Y){
+		x = X;
+		y = Y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+}
