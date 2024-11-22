2f308ce2ec3179ccb7fd10400723cc04f1281366
MoSelasie <108753891+MoSelasie@users.noreply.github.com>
2024-11-22 10:50:03 -0500

    Update scout piece logic
    inherits from piece, but would have to include logic that gives it extra moves.

--- stepping-stones/Scripts/Scout.cs
+++ stepping-stones/Scripts/Scout.cs
@@ -1,8 +1,38 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-
-public interface Scout : Piece
+//To Do: implement toString, and movement; improve checkLegalMove logic
+public partial class Scout : Piece
 {
+	enum team{};
+	private scoutTeam;
+	private currentLocation;
+	public Scout (Team team, Location startingLocation) {
+		scoutTeam = team;
+		currentLocation = startingLocation;
+	}
 	
+	abstract String toString();
+ 	abstract List<Location> move(Board b, Location location); 
+	
+	abstract bool moveIsLegal(Board b, Location startingLocation, Location destination){
+		//check if destination is within bounds. Note: add "check board dimensions to board class"
+		
+		if (b.getPiece(destination) != null) {
+			return false;
+		}
+		
+		deltaX = Math.Abs(startingLocation.getX() - destination.getX());
+		deltaY = Math.Abs(startingLocation.getY() - destination.getY());
+		
+		return (deltaX == 1 || deltaY == 1);
+	}
+	
+	Location getLocation() {
+		return currentLocation;
+	}
+	
+	Location setLocation(Location location) {
+		currentLocation = location;
+	}
 }
