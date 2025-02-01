857893cd6ddc43619ce345d8f6abe16946761a17
Taylor Hemby-Ginsberg <98064323+ThembyG@users.noreply.github.com>
2025-02-01 12:26:09 -0500

    Minor Tweaks
    Updated GameSaver to use Location instead of Board.Position

--- stepping-stones/Scripts/SaveLoad/GameSaver.cs
+++ stepping-stones/Scripts/SaveLoad/GameSaver.cs
@@ -21,11 +21,11 @@ public class GameSaver : FileSaver
 		#nullable enable
 		for (int i = 0; i < size[0]; i++) for (int j = 0; j < size[1]; j++) {
 			String? entry = null;
-			Tile? currTile = board.tileAt(new Board.Position(i ,j)); 
+			Tile? currTile = board.tileAt(new Location(i ,j)); 
 			if (currTile != null) {
 				char color = (currTile.color() == Piece.Color.PLAYER_1) ? 'r' : 'b';
 				entry = "" + ('a' + i) + j + '=' + color + 't';
-				Scout? currScout = board.scoutAt(new Board.Position(i,j));
+				Scout? currScout = board.scoutAt(new Location(i,j));
 				if (currScout != null) {
 					entry += 's';
 				}
@@ -43,19 +43,19 @@ public class GameSaver : FileSaver
 		FileAccess gameFile = FileAccess.Open(fileName, FileAccess.ModeFlags.Read);
 		int[] size = Array.ConvertAll(gameFile.GetLine().Split(' '), int.Parse);
 		SteppingStonesBoard board = new GridSteppingStonesBoard(size[0], size[1]);
-		board.removeTile(new Board.Position(size[1] / 2, 1));
-		board.removeTile(new Board.Position(size[1] / 2, size[0] - 2));
-		board.removeScout(new Board.Position(size[1] / 2, 1));
-		board.removeScout(new Board.Position(size[1] / 2, size[0] - 2));
+		board.removeTile(new Location(size[1] / 2, 1));
+		board.removeTile(new Location(size[1] / 2, size[0] - 2));
+		board.removeScout(new Location(size[1] / 2, 1));
+		board.removeScout(new Location(size[1] / 2, size[0] - 2));
 		while (gameFile.GetPosition() < gameFile.GetLength()) {
 			String line = gameFile.GetLine();
 			int row = line[0] - 'a';
 			int col = line[1] - '0';
 			Piece.Color color = (line[3] == 'r') ? Piece.Color.PLAYER_1 : Piece.Color.PLAYER_2;
 			Tile currTile = new Tile(color);
-			board.addTile(currTile, new Board.Position(row, col));
+			board.addTile(currTile, new Location(row, col));
 			if (line.Length == 5) {
-				board.addScout(new Scout(color), new Board.Position(row, col));
+				board.addScout(new Scout(color), new Location(row, col));
 			}
 		}	
 		return board;
