cbcd3d1c5be893f335ba182e9edf5d5f101c090a
Taylor Hemby-Ginsberg <98064323+ThembyG@users.noreply.github.com>
2025-02-13 20:55:48 -0500

    Mac Build and Some minor tweaks to reset

--- stepping-stones/Scripts/UILogic/MainGame.cs
+++ stepping-stones/Scripts/UILogic/MainGame.cs
@@ -12,7 +12,6 @@ public partial class MainGame : Node2D
 	public override void _Ready()
 	{
 		manager = GetNode<BoardManager>("Main/BoardManager");
-		display = GetNode<BoardDisplay>("Main/BoardManager/Board");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,29 +22,13 @@ public partial class MainGame : Node2D
 	public void OnUISaveGame(String path) {
 		saver.SaveGame(manager.board(), path);
 	}
-
+	public void OnUIResetGame() {
+		manager.onRestart();
+	}
 	public void OnUILoadGame(String path) {
 		GD.Print("Game Loaded");
 		manager.setBoard(saver.LoadGame(path));
 		Board board = manager.board();
 		int[] size = manager.board().size();
-		
-		#nullable enable
-		for (int i = 0; i < size[0]; i++) for (int j = 0; j < size[1]; j++) {
-			String entry = "";
-			Tile? currTile = board.tileAt(Location.at(i ,j)); 
-			if (currTile != null) {
-				char color = (currTile.color() == Piece.Color.PLAYER_1) ? 'r' : 'b';
-				entry = "" + (char)('a' + i) + (j + 1) + '=' + color + 't';
-				Scout? currScout = board.scoutAt(Location.at(i,j));
-				if (currScout != null) {
-					entry += 's';
-				}
-				if(entry != null) {
-					GD.Print(entry);
-				}
-			}
-		}
-		#nullable disable
 	}
 }
