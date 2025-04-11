465dfcce65b40d46c3fbd442d0af27130d0650c2
JlBrockett <jlbrockett@gmail.com>
2025-04-10 20:38:39 -0400

    Phase tests

--- stepping-stones/test/Scripts/Board/Phase_Test.cs
+++ stepping-stones/test/Scripts/Board/Phase_Test.cs
@@ -15,6 +15,7 @@ namespace GdUnitDefaultTestNamespace
 		private BoardManager manager;
 		private GridSteppingStonesBoard board;
 		private Piece.Color p1, p2;
+		private EventBus _eventBus = EventBus.Bus;
 
 
 
@@ -39,6 +40,9 @@ namespace GdUnitDefaultTestNamespace
 
 			manager.onCellSelection(p1, 0, 0);
 			manager.onCellSelection(p2, 0, 1);
+
+			manager.onCellSelection(p1, 0, 2);
+
 			Assertions.AssertThat(manager.phase()).IsEqual(BoardManager.GamePhase.MOVE);
 
 
@@ -47,15 +51,18 @@ namespace GdUnitDefaultTestNamespace
 		[TestCase]
 		public void placeToMoveTurnOrderP1()
 		{
-			manager.setTileCount(p1, 1);
-			manager.setTileCount(p2, 1);
+			manager.setTileCount(p1, 0);
+			manager.setTileCount(p2, 0);
 			Assertions.AssertThat(manager.phase()).IsEqual(BoardManager.GamePhase.PLACE);
 			Assertions.AssertThat(manager.playerTurn()).IsEqual(p1);
 
 			manager.onCellSelection(p1, 0, 0);
 			manager.onCellSelection(p2, 0, 1);
+
+			manager.onCellSelection(p1, 0, 2); //needs selection to move to move phase)
 			Assertions.AssertThat(manager.phase()).IsEqual(BoardManager.GamePhase.MOVE);
 			Assertions.AssertThat(manager.playerTurn()).IsEqual(p1);
+			//did not complete movement, so should still be p1 turn
 
 		}
 
@@ -70,6 +77,8 @@ namespace GdUnitDefaultTestNamespace
 
 			manager.onCellSelection(p2, 0, 1);
 			manager.onCellSelection(p1, 0, 0);
+
+			manager.onCellSelection(p2, 0,0);
 			Assertions.AssertThat(manager.phase()).IsEqual(BoardManager.GamePhase.MOVE);
 			Assertions.AssertThat(manager.playerTurn()).IsEqual(p2);
 
@@ -77,22 +86,25 @@ namespace GdUnitDefaultTestNamespace
 
 		[TestCase]
 		public void placeToMoveTilePlacement(){
-			manager.setTileCount(p1, 2);
-			manager.setTileCount(p2, 2);
+			manager.setTileCount(p1, 1);
+			manager.setTileCount(p2, 1);
 			Assertions.AssertThat(manager.phase()).IsEqual(BoardManager.GamePhase.PLACE);
 
 			manager.onCellSelection(p1, 0, 0);
+			Assertions.AssertThat(manager.board().tileAt(Location.at(0,0)).color()).IsEqual(p1);
+
 			manager.onCellSelection(p2, 0, 1);
+			Assertions.AssertThat(manager.board().tileAt(Location.at(0,1)).color()).IsEqual(p2);
+
 			manager.onCellSelection(p1, 0, 2);
-			manager.onCellSelection(p2, 0, 3);
 			Assertions.AssertThat(manager.phase()).IsEqual(BoardManager.GamePhase.MOVE);
 
 
 			for (int i = 0; i < 5; i ++){
-				for (int j = 0; j < 7; i++){
+				for (int j = 0; j < 7; j++){
 					Location currLoc = Location.at(i, j);
 					Tile currTile = manager.board().tileAt(currLoc);
-					if ((i == 0 && j < 4)){
+					if ((i == 0 && j < 2)){
 						if (j%2 == 0){
 							Assertions.AssertThat(currTile.color()).IsEqual(p1);
 						} else {
@@ -121,17 +133,90 @@ namespace GdUnitDefaultTestNamespace
 		//[TestCase]
 		//public void restartToPlaceIsAutomatic(){}
 
-		//[TestCase]
-		//public void moveToWinScoutPass(){}
+		[TestCase]
+		public async Task moveToWinScoutAndTilePass(){
+			Tile winningTile = new Tile(p2);
+			Scout winningScout = new Scout(p2);
+			manager.board().addTile(winningTile, Location.at(3, 1));
+			manager.board().addScout(winningScout, Location.at(3, 1));
+			
+			manager.setPhase(BoardManager.GamePhase.MOVE);
+			manager.setTurn(p2);
 
-		//[TestCase]
-		//public void moveToWinScoutKnockout(){}
+			AssertSignal(_eventBus).StartMonitoring();
+			AssertSignal(_eventBus).IsSignalExists("onPlayerWin");
+			manager.onCellSelection(p2, 3, 1);
+			manager.onCellSelection(p2, 3, 0);
 
-		//[TestCase]
-		//public void moveToWinScoutKnockoutAndPass(){}
+			await AssertSignal(_eventBus).IsEmitted("onPlayerWin").WithTimeout(50);
 
-		//[TestCase]
-		//public void moveToWinPushedIntoZone(){}
+			Assertions.AssertThat(manager.board().scoutAt(Location.at(3, 0))).IsNotNull();
+		}
+
+		[TestCase]
+		public async Task moveToWinScoutKnockout(){
+			for (int i = 0; i < 3; i++){
+				Location currLoc = Location.at(1+i, 1);
+				manager.board().addTile(new Tile(p2), currLoc);
+			}
+			manager.board().addScout(new Scout(p1), Location.at(0, 1));
+
+			manager.board().scoutLayer()[2, 1] = null;
+
+			AssertSignal(_eventBus).StartMonitoring();
+
+			manager.setPhase(BoardManager.GamePhase.MOVE);
+			manager.setTurn(p2);
+			manager.onCellSelection(p2, 3, 1);
+			manager.onCellSelection(p2, -1, 1);
+
+			Assertions.AssertThat(manager.board().tileAt(Location.at(3,1))).IsNull();
+
+			await AssertSignal(_eventBus).IsEmitted("onPlayerWin").WithTimeout(50);
+
+		}
+
+		[TestCase]
+		public async Task moveToWinScoutKnockoutAndPass(){
+			for (int i = 0; i < 3; i++){
+				Location currLoc = Location.at(2, 2+i);
+				manager.board().addTile(new Tile(p2), currLoc);
+			}
+
+			AssertSignal(_eventBus).StartMonitoring();
+
+			manager.setPhase(BoardManager.GamePhase.MOVE);
+			manager.setTurn(p2);
+			manager.onCellSelection(p2, 2, 4);
+			manager.onCellSelection(p2, 2, 0);
+			manager.setTurn(p2);
+			manager.onCellSelection(p2, 2, 3);
+			manager.onCellSelection(p2, 2, -1);
+
+			await AssertSignal(_eventBus).IsEmitted("onPlayerWin").WithTimeout(50);
+
+		}
+
+		[TestCase]
+		public async Task moveToWinPushedIntoZone(){
+			AssertSignal(_eventBus).StartMonitoring();
+
+			for (int i = 0; i < 3; i++){
+				Location currLoc = Location.at(3, 2+i);
+				manager.board().addTile(new Tile(p1), currLoc);
+			}
+			manager.board().addScout(new Scout(p2), Location.at(3, 1));
+
+			manager.setPhase(BoardManager.GamePhase.MOVE);
+			manager.setTurn(p1);
+
+			manager.onCellSelection(p1, 3, 4);
+			manager.onCellSelection(p1, 3, 0);
+
+			Assertions.AssertThat(manager.board().scoutAt(Location.at(3,0))).IsNotNull();
+
+			await AssertSignal(_eventBus).IsEmitted("onPlayerWin").WithTimeout(50);
+		}
 
 	}
 }
